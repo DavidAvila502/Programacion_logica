a3aa88fce4cd11c2e812ae2b0352ab8e6b0d3a0c
David Avila <davidavila5002@gmail.com>
2021-10-15 17:23:35 -0500

    Primer programa de la materia

---
+++ dolares_a_pesos.py
@@ -0,0 +1,14 @@
+# Tarea 01
+# Crear transforme los dolares a pesos mexicanos e imprima en consola el siguiente texto
+# Tus dolares (cantidad_usd) USD son (cantidad_mx) MX
+
+valor_dolar = 20.45
+
+cantidad_dolares = float(input("Ingrese los USD que tienes: "))
+
+cantidad_pesos = valor_dolar * cantidad_dolares
+
+
+print("Tus dolares %s USD son %s MX" %(cantidad_dolares,cantidad_pesos))
+
+
